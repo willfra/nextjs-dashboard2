6a8348841455635263f851927afc3a30225e560f
willfroso <will.franca1998@gmail.com>
2024-09-22 13:10:56 -0300

    Changes to be committed: 	modified:   app/dashboard/(overview)/page.tsx 	modified:   app/dashboard/invoices/page.tsx 	modified:   app/ui/dashboard/cards.tsx 	modified:   app/ui/dashboard/latest-invoices.tsx 	modified:   app/ui/dashboard/revenue-chart.tsx 	modified:   app/ui/dashboard/sidenav.tsx 	modified:   app/ui/search.tsx

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,6 @@
 import { generateYAxis } from "@/app/lib/utils";
 import { CalendarIcon } from "@heroicons/react/24/outline";
-import { lusitana } from "@/app/ui/fonts";
+import { inter } from "@/app/ui/fonts";
 import { fetchRevenue } from "@/app/lib/data";
 
 // This component is representational only.
@@ -24,7 +24,7 @@ export default async function RevenueChart() {
 
     return (
         <div className="w-full md:col-span-4">
-            <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+            <h2 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
                 Recent Revenue
             </h2>
             {/* NOTE: Uncomment this code in Chapter 7 */}
