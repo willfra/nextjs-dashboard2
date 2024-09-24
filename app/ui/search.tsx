e5c6547321b08fa8a1e539106ca2805087ceb767
willfroso <will.franca1998@gmail.com>
2024-09-23 23:12:59 -0300

    CAPITULO 12 Changes to be committed: 	new file:   app/dashboard/invoices/[id]/edit/page.tsx 	new file:   app/dashboard/invoices/create/page.tsx 	modified:   app/dashboard/invoices/page.tsx 	new file:   app/lib/actions.ts 	modified:   app/ui/invoices/buttons.tsx 	modified:   app/ui/invoices/create-form.tsx 	modified:   app/ui/invoices/edit-form.tsx 	modified:   app/ui/invoices/pagination.tsx 	modified:   app/ui/search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,19 +7,18 @@ import { useDebouncedCallback } from "use-debounce";
 export default function Search({ placeholder }: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
-    
     const { replace } = useRouter();
-      const handleSearch = useDebouncedCallback((term) => {
-        console.log(`Searching... ${term}`);
 
+    const handleSearch = useDebouncedCallback((term) => {
         const params = new URLSearchParams(searchParams);
+        params.set('page', '1')
         if (term) {
-          params.set('query', term);
+            params.set("query", term);
         } else {
-          params.delete('query');
+            params.delete("query");
         }
         replace(`${pathname}?${params.toString()}`);
-      }, 300);
+    }, 300);
 
     return (
         <div className="relative flex flex-1 flex-shrink-0">
